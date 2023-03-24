ba87ff44b2f098665c4843eb7017a870c642d819
mohamed271220 <mohamedaprahym@gmail.com>
2023-03-24 03:29:46 +0200

    Best 100 movies of all time made into both csv and txt

--- main.py
+++ main.py
@@ -1,52 +1,84 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+
+# ///////////////////////////////  100 movies scarpping ///////////////////////////////
 
 
-response=requests.get("https://news.ycombinator.com/news")
-yc_web_page=response.text
-# print(yc_web_page)
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-soup= BeautifulSoup(yc_web_page,'html.parser')
+response = requests.get(URL)
 
-print(soup.title)
-head_line=soup.find(name='span',class_='titleline')
-print(head_line.getText())
+empire_page = response.text
 
-article_link= soup.select_one('.titleline a').get('href')
-print(article_link)
-# or
-# article_link=head_line.get('href')
+# print(empire_page)
+
+
+soup = BeautifulSoup(empire_page, 'html.parser')
+
+# using list comprehension
+headlines = [item.getText() for item in soup.select('h3.title')]
+
+headlines.reverse()
+# == headlines[::-1]
+
+print(headlines)
+
+# make a csv with a list
+df = pd.DataFrame(headlines, columns=["Best movies ever"])
+df.to_csv('movies.csv', index=False)
+
+# make a txt file
+with open('movies.txt' , mode='w',encoding="utf-8") as file:
+    for movie in headlines:
+        file.write(f'{movie}\n')
+
+# myFile = open('', 'r+')
+# writer = csv.writer(myFile)
+# writer.writerow(headlines)
+# myFile.close()
+
+
+# ///////////////////// scarpping hacker news ///////////////////////////
+# response = requests.get("https://news.ycombinator.com/news")
+# yc_web_page = response.text
+# # print(yc_web_page)
+#
+# soup = BeautifulSoup(yc_web_page, 'html.parser')
+#
+# print(soup.title)
+# head_line = soup.find(name='span', class_='titleline')
+# print(head_line.getText())
+#
+# article_link = soup.select_one('.titleline a').get('href')
 # print(article_link)
-
-article_upvote= soup.select_one('span .score').getText()
-print(article_upvote)
-
-article_links=[]
-article_texts=[]
-
-all_anchour_tags=soup.select('.titleline a')
-for tag in all_anchour_tags:
-    article_texts.append(tag.getText())
-    article_links.append(tag.get('href'))
-
-article_upvotes=[ int(score.getText().split()[0]) for score in soup.select('span .score')]
-
-print(f'{article_texts}\n{article_links}\n{article_upvotes}')
-
-
-largest_number=max(article_upvotes)
-
-
-largest_index=article_upvotes.index(largest_number)
-
-print(f'{article_texts[largest_index]}\n{article_links[largest_index]}')
-
-
-
+# # or
+# # article_link=head_line.get('href')
+# # print(article_link)
+#
+# article_upvote = soup.select_one('span .score').getText()
+# print(article_upvote)
+#
+# article_links = []
+# article_texts = []
+#
+# all_anchour_tags = soup.select('.titleline a')
+# for tag in all_anchour_tags:
+#     article_texts.append(tag.getText())
+#     article_links.append(tag.get('href'))
+#
+# article_upvotes = [int(score.getText().split()[0]) for score in soup.select('span .score')]
+#
+# print(f'{article_texts}\n{article_links}\n{article_upvotes}')
+#
+# largest_number = max(article_upvotes)
+#
+# largest_index = article_upvotes.index(largest_number)
+#
+# print(f'{article_texts[largest_index]}\n{article_links[largest_index]}')
 
 
-
-
+# ////////////////////////////// 101 scrapping ///////////////////////////////////
 
 # with open('website.html', encoding="utf8") as file:
 #     contents = file.read()
